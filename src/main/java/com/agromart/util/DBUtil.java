c73d76580f5a5af2654b79002265dba71fd285e3
Lakhan Patidar <lakhanpatidar7316@gmail.com>
2026-04-16 16:48:34 +0530

    Initial project setup with DB connection

---
+++ src/main/java/com/agromart/util/DBUtil.java
@@ -0,0 +1,61 @@
+package com.agromart.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class DBUtil {
+	private final String URL="jdbc:mysql://localhost:3306/agromart";
+	private final String USERNAME="root";
+	private final String PASSWORD="root@123";
+	private final String DRIVER="com.mysql.cj.jdbc.Driver";
+
+
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName(DRIVER);
+		Connection con =DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		System.out.println("//\\Namastey Padharo Saa...");
+		return con;
+	}
+	public void close(Connection con ,PreparedStatement pstmt , ResultSet rs) throws SQLException {
+	if(rs!=null)
+		rs.close();
+	if(pstmt!=null)
+		pstmt.close();
+	if(con!=null)
+		con.close();
+	}
+	public void close( ResultSet rs) throws SQLException {
+	close(null, null, rs);
+	}
+	public void close(Connection con ,PreparedStatement pstmt) throws SQLException {
+	close(con, pstmt, null);
+	}
+	public void close(PreparedStatement pstmt , ResultSet rs) throws SQLException {
+	close(null, pstmt, rs);
+	}
+	public void close(PreparedStatement pstmt) throws SQLException {
+	close(null, pstmt, null);
+	}
+	public void close(Connection con ) throws SQLException {
+	close(con, null, null);
+	}
+	public void close(Connection con , ResultSet rs) throws SQLException {
+	close(con, null, rs);
+	}
+	public static void main(String[] args) {
+		DBUtil dbutil =new DBUtil();
+		try {
+			dbutil.getConnection();
+		}catch (ClassNotFoundException e) {
+		e.printStackTrace();
+		}catch (SQLException e) {
+	          e.printStackTrace();
+		}
+	}
+
+
+}
